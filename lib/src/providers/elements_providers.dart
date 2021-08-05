a974c8906994d464589a770b39ded9ebe1c8b955
Mauricio Gael Hernandez Lom <mhernandezl1605@alumno.ipn.mx>
2021-08-05 04:41:44 -0500

    I create the elements_provider to get the information

--- lib/src/providers/elements_providers.dart
+++ lib/src/providers/elements_providers.dart
@@ -1,1 +1,31 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:flutter/services.dart' show rootBundle;
+
+
+class _ElementsProvider {
+
+  List <dynamic> elements = [];
+
+  _ElementsProvider(){
+    //cargarData();
+  } //Es el construcor de la clase, aunque esta vacia porque el método ya está realizando la asignación de datos del Json
+
+//Lo que estamos haciendo es volver asincrona la tarea para posteriormente en el home_page.dart hacerle un future a las opciones para que se cargen solo hasta que la tarea quedo completada.
+Future<List<dynamic>> cargarData() async{
+
+    final resp= await rootBundle.loadString('data/data.json');
+
+    //Map dataMap = json.decode( resp );  //estamos convirtiendo al json en un map con la función decode
+    //elements = dataMap['rutas'];
+    List<Map<String,dynamic>> data = json.decode(resp);
+
+    elements = data;
+
+    return elements;
+  }
+
+}
+
+
+final elementsProvider = new  _ElementsProvider();  //Solo expone la instancia creada de la clase  
